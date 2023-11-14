f84db3ac8c9cc130966bdf22d27ae7a48ab327aa
Charlie Cheesman <djnr.cheesman@gmail.com>
2023-11-14 15:54:28 +0000

    adding completed exercise with nice formatting

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,11 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+10.times do
+  Task.create(
+    title: Faker::Name.name,
+    details: Faker::Address.street_address,
+    completed: false
+  )
+end
